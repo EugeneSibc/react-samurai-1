cd2a749eeac3912137947fd703e7d91367a15767
eugeneman <evgensbk9@gmail.com>
2022-11-12 15:47:13 +0700

    commit-1

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -20,7 +20,7 @@ class ProfileContainer extends React.Component{
     componentDidMount() {
         let userId = this.props.router.params.userId;
         if(!userId){
-            userId = 2;
+            userId = 0;
         }
         axios.get('https://social-network.samuraijs.com/api/1.0/profile/' + userId)
             .then(response=>{
