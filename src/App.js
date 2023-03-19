1a474e1ec71382221aaf54a483755e9ac823c818
eugeneman <evgensbk9@gmail.com>
2023-03-19 15:50:19 +0700

    add React.lazy and React.Suspense

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
 import {Route, Routes} from "react-router-dom";
 import Settings from "./components/Settings/Settings";
 import Photo from "./components/Photo/Photo";
 import Mussic from "./components/Mussic/Mussic";
 import News from "./components/News/News";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
+
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
-import themePicture from './assets/images/community.jpg';
 import {useLocation, useNavigate, useParams} from "react-router";
-
 import {connect} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 
-
+// import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
+// import ProfileContainer from "./components/Profile/ProfileContainer";
+const DialogsContainer = React.lazy(()=>import("./components/Dialogs/DialogsContainer"));
+//const UsersContainer = React.lazy(()=>import("./components/Users/UsersContainer"));
+const ProfileContainer = React.lazy(()=>import("./components/Profile/ProfileContainer"));
 
 class App extends React.Component {
     componentDidMount() {
@@ -36,12 +35,15 @@ class App extends React.Component {
                 <HeaderContainer/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
+                    <Suspense fallback={<Preloader/>}>
+                        <Routes>
+                            <Route exact path='/dialogs' element={<DialogsContainer/>}/>
+                            <Route path='/profile/:userId' element={<ProfileContainer/>}/>
+                            <Route path='/profile' element={<ProfileContainer/>}/>
+
+                        </Routes>
+                    </Suspense>
                     <Routes>
-                        <Route path='/profile/:userId' element={<ProfileContainer/>}/>
-                        <Route path='/profile' element={<ProfileContainer/>}/>
-                        <Route exact path='/dialogs' element={<DialogsContainer
-                            /*data={props.state.dialogsPage}
-                            dispatch={props.dispatch}*//>}/>
                         <Route path='/users' element={<UsersContainer/>}/>
                         <Route path='/photo' element={<Photo/>}/>
                         <Route path='/settings' element={<Settings/>}/>
@@ -49,6 +51,7 @@ class App extends React.Component {
                         <Route path='/news' element={<News/>}/>
                         <Route path='/login' element={<Login/>}/>
                     </Routes>
+
                 </div>
 
             </div>
